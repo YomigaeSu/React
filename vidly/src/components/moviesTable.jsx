f5a3b563717eaf74c3e1986e8ee0fd0e31fdc25e
YomigaeSu <mn.yy@163.com>
2018-12-23 15:17:13 +1100

    Update TableBody

--- vidly/src/components/moviesTable.jsx
+++ vidly/src/components/moviesTable.jsx
@@ -60,29 +60,6 @@ class MoviesTable extends Component {
           onSort={onSort}
         />
         <TableBody items={movies} columns={this.columns} />
-        {/* <tbody>
-          {movies.map(movie => (
-            <tr key={movie._id}>
-              <td>{movie.title}</td>
-              <td>{movie.genre.name}</td>
-              <td>{movie.numberInStock}</td>
-              <td>{movie.dailyRentalRate}</td>
-              <td>
-                <LikeButton liked={movie.liked} onClick={() => onLike(movie)} />
-              </td>
-              <td>
-                <button
-                  className="btn btn-danger btn-sm"
-                  onClick={() => {
-                    onDelete(movie._id);
-                  }}
-                >
-                  Delete
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody> */}
       </table>
     );
   }
