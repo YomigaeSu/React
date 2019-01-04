c2f300ec9153ff509af5c43ed93d6ffdc7dda5c8
YomigaeSu <mn.yy@163.com>
2019-01-04 22:14:53 +1100

    Extracted Table Component

---
+++ vidly/src/components/common/table.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import TableHeader from "./tableHeader";
+import TableBody from "./tableBody";
+
+class Table extends Component {
+  render() {
+    const { columns, sortColumn, onSort, items } = this.props;
+
+    return (
+      <table className="table">
+        <TableHeader
+          columns={columns}
+          sortColumn={sortColumn}
+          onSort={onSort}
+        />
+        <TableBody items={items} columns={columns} />
+      </table>
+    );
+  }
+}
+
+export default Table;
