44466896a46e981d84edb62132f8e44e1a189f32
YomigaeSu <mn.yy@163.com>
2019-01-07 18:18:13 +1100

    Maded parameters optional

--- router-app/src/components/posts.jsx
+++ router-app/src/components/posts.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 
-const Posts = () => {
+const Posts = ({ match }) => {
+  // this is a sfc, match needs to be passed to this function
   return (
     <div>
       <h1>Posts</h1>
-      Year: , Month:
+      Year: {match.params.year}, Month: {match.params.month}
     </div>
   );
 };
