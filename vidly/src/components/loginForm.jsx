e867108d6fe4df006a6bc6f744e1e50b5b94715d
YomigaeSu <mn.yy@163.com>
2019-01-12 00:27:35 +1100

    Added Ref&AutoFocus

--- vidly/src/components/loginForm.jsx
+++ vidly/src/components/loginForm.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 class LoginForm extends Component {
+  username = React.createRef();
+
   handleSubmit = e => {
     e.preventDefault();
 
@@ -13,7 +15,13 @@ class LoginForm extends Component {
         <form onSubmit={this.handleSubmit}>
           <div className="form-group">
             <label htmlFor="username">Username</label>
-            <input id="username" type="text" className="form-control" />
+            <input
+              autoFocus
+              ref={this.username}
+              id="username"
+              type="text"
+              className="form-control"
+            />
           </div>
           <div className="form-group">
             <label htmlFor="password" />
