32638a496ca36ff167ff74de8a445e0d136db1b2
YomigaeSu <mn.yy@163.com>
2019-01-11 23:37:02 +1100

    Added Login Form

--- vidly/src/App.js
+++ vidly/src/App.js
@@ -6,26 +6,31 @@ import Movies from "./components/movies";
 import Customers from "./components/customers";
 import Rentals from "./components/rentals";
 import NotFound from "./components/notFound";
+import MovieForm from "./components/movieForm";
+import LoginForm from "./components/loginForm";
 
 import "./App.css";
-import MovieForm from "./components/movieForm";
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <React.Fragment>
         <NavBar />
-        <Switch>
-          {/* without Switch, all address will be redirected to "/" */}
-          <Route path="/movies/:id" component={MovieForm} />
-          <Route path="/movies" component={Movies} />
-          <Route path="/customers" component={Customers} />
-          <Route path="/rentals" component={Rentals} />
-          <Route path="/not-found" component={NotFound} />
-          <Redirect from="/" exact to="/movies" />
-          <Redirect to="/not-found" />
-        </Switch>
-      </div>
+
+        <main class="container">
+          <Switch>
+            {/* without Switch, all address will be redirected to "/" */}
+            <Route path="/login" component={LoginForm} />
+            <Route path="/movies/:id" component={MovieForm} />
+            <Route path="/movies" component={Movies} />
+            <Route path="/customers" component={Customers} />
+            <Route path="/rentals" component={Rentals} />
+            <Route path="/not-found" component={NotFound} />
+            <Redirect from="/" exact to="/movies" />
+            <Redirect to="/not-found" />
+          </Switch>
+        </main>
+      </React.Fragment>
     );
   }
 }
