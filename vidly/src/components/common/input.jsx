798de5ee5b1de45202bbc1563a5df07f8b66f2b1
YomigaeSu <mn.yy@163.com>
2019-02-06 20:53:47 +1100

    Added rest spreader

--- vidly/src/components/common/input.jsx
+++ vidly/src/components/common/input.jsx
@@ -1,16 +1,9 @@
 import React from "react";
-const Input = ({ name, label, value, onChange, error, type }) => {
+const Input = ({ name, label, error, ...rest }) => {
   return (
     <div className="form-group">
       <label htmlFor="name">{label}</label>
-      <input
-        id={name}
-        onChange={onChange}
-        name={name}
-        value={value}
-        type={type}
-        className="form-control"
-      />
+      <input {...rest} id={name} name={name} className="form-control" />
       {error && <div className="alert alert-danger">{error}</div>}
     </div>
   );
