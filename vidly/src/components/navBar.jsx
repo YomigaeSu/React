a712097d8ba6cfc72e937a7b481d27466b04e78c
YomigaeSu <mn.yy@163.com>
2019-02-06 22:16:38 +1100

    Completed exercise RegisterForm

--- vidly/src/components/navBar.jsx
+++ vidly/src/components/navBar.jsx
@@ -24,16 +24,17 @@ class NavBar extends Component {
             <NavLink className="nav-item nav-link" to="/movies">
               Movies
             </NavLink>
-
             <NavLink className="nav-item nav-link" to="/customers">
               Customers
             </NavLink>
-
             <NavLink className="nav-item nav-link" to="/rentals">
               Rentals
             </NavLink>
             <NavLink className="nav-item nav-link" to="/login">
               Login
+            </NavLink>
+            <NavLink className="nav-item nav-link" to="/register">
+              Register
             </NavLink>
           </div>
         </div>
