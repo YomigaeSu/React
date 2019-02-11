205bd2c594de79bafe9a3dcbd1a75d86c51a15ba
YomigaeSu <mn.yy@163.com>
2019-02-11 21:43:00 +1100

    Deleted finished http app

--- http-app/src/App.js
+++ http-app/src/App.js
@@ -1,8 +1,4 @@
 import React, { Component } from "react";
-import { ToastContainer } from "react-toastify";
-import http from "./services/httpService";
-import config from "./config.json";
-import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 
 class App extends Component {
@@ -10,49 +6,21 @@ class App extends Component {
     posts: []
   };
 
-  async componentDidMount() {
-    // pending > resolved (success) OR rejected (failure)
-    const { data: posts } = await http.get(config.apiEndpoint);
-    this.setState({ posts });
-  }
-
-  handleAdd = async () => {
-    const obj = { title: "a", body: "b" };
-    const { data: post } = await http.post(config.apiEndpoint, obj);
-
-    const posts = [post, ...this.state.posts];
-    this.setState({ posts });
+  handleAdd = () => {
+    console.log("Add");
   };
 
-  handleUpdate = async post => {
-    post.title = "UPDATED";
-    await http.put(config.apiEndpoint + "/" + post.id, post);
-
-    const posts = [...this.state.posts];
-    const index = posts.indexOf(post);
-    posts[index] = { ...post };
-    this.setState({ posts });
+  handleUpdate = post => {
+    console.log("Update", post);
   };
 
-  handleDelete = async post => {
-    const originalPosts = this.state.posts;
-
-    const posts = this.state.posts.filter(p => p.id !== post.id);
-    this.setState({ posts });
-
-    try {
-      await http.delete(config.apiEndpoint + "/" + post.id);
-    } catch (ex) {
-      if (ex.response && ex.response.status === 404)
-        alert("This post has already been deleted.");
-      this.setState({ posts: originalPosts });
-    }
+  handleDelete = post => {
+    console.log("Delete", post);
   };
 
   render() {
     return (
       <React.Fragment>
-        <ToastContainer />
         <button className="btn btn-primary" onClick={this.handleAdd}>
           Add
         </button>
