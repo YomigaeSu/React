3bbe2656ce5eb8bdd6f82aba0577bd681a1aac28
YomigaeSu <mn.yy@163.com>
2019-02-12 17:13:35 +1100

    Imported Sentry error logging

--- http-app/src/index.js
+++ http-app/src/index.js
@@ -4,7 +4,12 @@ import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
-import Raven from "raven-js";
+
+import * as Sentry from "@sentry/browser";
+
+Sentry.init({
+  dsn: "https://c7960d1467574dab8d30878b9570b274@sentry.io/1391980"
+});
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
