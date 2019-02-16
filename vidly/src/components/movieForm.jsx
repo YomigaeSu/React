f69c0ad9fa473d04c91370842e9c9d7066da8507
YomigaeSu <mn.yy@163.com>
2019-02-17 00:22:58 +1100

    Refactored cdm

--- vidly/src/components/movieForm.jsx
+++ vidly/src/components/movieForm.jsx
@@ -37,20 +37,26 @@ class MovieForm extends Form {
       .label("Daily Rental Rate")
   };
 
-  async componentDidMount() {
+  async populateGenres() {
     const { data: genres } = await genreServiceAPI.getGenres();
     this.setState({ genres });
+  }
 
-    const movieId = this.props.match.params.id;
-    if (movieId === "new") return;
-
+  async populateMovies() {
     try {
+      const movieId = this.props.match.params.id;
+      if (movieId === "new") return;
       const { data: movie } = await movieServiceAPI.getMovie(movieId);
       this.setState({ data: this.mapToViewModel(movie) });
     } catch (ex) {
       if (ex.response && ex.response.status === 404)
         this.props.history.replace("/not-found");
     }
+  }
+
+  async componentDidMount() {
+    await this.populateGenres();
+    await this.populateMovies();
   }
 
   mapToViewModel = movie => {
