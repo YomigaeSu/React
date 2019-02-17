3d7073c601ce0673e0f6999a8222f3b82a6b64e6
YomigaeSu <mn.yy@163.com>
2019-02-18 00:46:04 +1100

    Created registerService, added register error

--- vidly/src/components/registerForm.jsx
+++ vidly/src/components/registerForm.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Joi from "joi-browser";
 import Form from "./common/form";
+import { register } from "../services/userService";
 
 class RegisterForm extends Form {
   state = {
@@ -26,7 +27,18 @@ class RegisterForm extends Form {
       .label("Name")
   };
 
-  doSubmit = () => {};
+  doSubmit = async () => {
+    try {
+      await register(this.state.data);
+    } catch (ex) {
+      if (ex.response && ex.response.status === 400) {
+        const errors = this.state.errors;
+        errors.username = ex.response.data;
+        //error message got from the server
+        this.setState({ errors });
+      }
+    }
+  };
 
   render() {
     return (
