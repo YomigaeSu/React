f2d52d5a198fe5ac904cb4a699ef79c93d4d7e70
YomigaeSu <mn.yy@163.com>
2019-02-20 10:02:43 +1100

    Added get user

--- vidly/src/App.js
+++ vidly/src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import { Route, Redirect, Switch } from "react-router-dom";
-
-import NavBar from "./components/navBar";
+import jwtDecode from "jwt-decode";
 import Movies from "./components/movies";
+import NavBar from "./components/navBar";
 import Customers from "./components/customers";
 import Rentals from "./components/rentals";
 import NotFound from "./components/notFound";
@@ -10,16 +10,24 @@ import MovieForm from "./components/movieForm";
 import LoginForm from "./components/loginForm";
 import RegisterForm from "./components/registerForm";
 import { ToastContainer } from "react-toastify";
-
-import "./App.css";
 import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
 
 class App extends Component {
+  state = {};
+
+  componentDidMount() {
+    try {
+      const jwt = localStorage.getItem("token");
+      const user = jwtDecode(jwt);
+      this.setState({ user });
+    } catch (error) {}
+  }
   render() {
     return (
       <React.Fragment>
         <ToastContainer />
-        <NavBar />
+        <NavBar user={this.state.user} />
         <main className="container">
           <Switch>
             {/* without Switch, all address will be redirected to "/" */}
